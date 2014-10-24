4d66a043f37fed901cc932e266d835287fdbf271
SamGiles <samuel.giles@ft.com>
2014-10-24 11:32:48 +0100

    Added test case for binding on primitive 'constructors'

--- polyfills/Function.prototype.bind/tests.js
+++ polyfills/Function.prototype.bind/tests.js
@@ -3,3 +3,8 @@ it("Should allow binding on native methods", function() {
 	var join = Array.prototype.join.bind(newThis);
 	expect(join(',')).to.be('1,2,3,4');
 });
+
+it("Should allow binding on primitive 'constructors'", function() {
+	expect(String(new (String.bind(null, 'test')))).to.be("test");
+});
+
